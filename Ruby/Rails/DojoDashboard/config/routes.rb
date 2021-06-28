c6ae5a51e82695b2213fd6ee49cb8b8181c288de
colbyjoe97 <dumonchelle123@gmail.com>
2021-06-28 16:15:11 -0400

    removed commented code

--- Ruby/Rails/DojoDashboard/config/routes.rb
+++ Ruby/Rails/DojoDashboard/config/routes.rb
@@ -2,19 +2,9 @@ Rails.application.routes.draw do
   # Students
   resources :students
   get 'dojos/:id/students/new' => 'students#new'
-  # get 'dojos/:dojoId/students/:studentId' => 'students#showStudent'
-  # get 'dojos/:dojoId/students/:studentId/edit' => 'students#editStudentPage'
   post 'dojos/students' => 'students#create'
   patch 'students/:id/edit' => 'students#update'
-  # delete 'dojos/:dojoId/students/:studentId' => 'students#delete'
 
   # # Dojos
   resources :dojos
-  # get 'dojos' => 'dojos#index'
-  # get 'dojos/new' => 'dojos#new'
-  # get 'dojos/:id' => 'dojos#view'
-  # get 'dojos/:id/edit' => 'dojos#editPage'
-  # patch 'dojos/:id' => 'dojos#update'
-  # post 'dojos' => 'dojos#create'
-  # delete 'dojos/:id' => 'dojos#destroy'
 end
