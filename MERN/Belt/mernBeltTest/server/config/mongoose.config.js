9db791508e7aed65e1d72bbfaf6fd7e32be81d6c
colbyjoe97 <dumonchelle123@gmail.com>
2021-07-06 12:46:46 -0400

    removed exam

--- MERN/Belt/mernBeltTest/server/config/mongoose.config.js
+++
@@ -1,7 +0,0 @@
-const mongoose = require('mongoose');
-
-mongoose.connect('mongodb://localhost/students', {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-}).then(() => console.log("Established a connection to the DB"))
-.catch(err => console.log("Something went wrong when connecting to the DB", err));
