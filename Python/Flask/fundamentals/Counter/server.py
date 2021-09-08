ce971b19af88fc0c07e787a2821058e0c0d278ba
colbyjoe97 <dumonchelle123@gmail.com>
2021-09-08 11:52:20 -0400

    finished another new assignment for next TA class

---
+++ Python/Flask/fundamentals/Counter/server.py
@@ -0,0 +1,18 @@
+from flask import Flask, render_template, redirect, request, session
+app = Flask(__name__)
+app.secret_key = 'keep it secret, keep it safe'
+@app.route('/')
+def index():
+    if 'count' in session:
+        session['count'] += 1
+    else:
+        session['count'] = 0
+    return render_template("index.html", count=session['count'])
+
+@app.route('/destroy')
+def destroy():
+    del session['count']
+    return redirect("/")
+
+if __name__ == "__main__":
+    app.run(debug=True)
