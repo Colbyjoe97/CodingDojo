e94fb9030c0bed13cf0e0f5bdce875ddf3fed2cf
colbyjoe97 <dumonchelle123@gmail.com>
2021-09-15 17:10:40 -0400

    removed unused code

--- Python(2021)/fundamentals/comparing_lists.py
+++ Python(2021)/fundamentals/comparing_lists.py
@@ -1,14 +1,10 @@
 list_one = [1,2,5,6,5,16]
 list_two = [1,2,5,6,5]
 
-
-
-
 def compare(list_one, list_two):
     if list_one == list_two:
         print("The lists are the same")
     else:
         print("The lists are different")
-
 
 compare(list_one, list_two)
