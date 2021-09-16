9840a54f9e3f8b79bcdb36e3f64c8f147f7efd1e
colbyjoe97 <dumonchelle123@gmail.com>
2021-09-16 15:04:06 -0400

    finished assignment

---
+++ Python(2021)/fundamentals/scores_and_grades.py
@@ -0,0 +1,17 @@
+import random
+def grades():
+    for i in range(1, 11):
+        randNum = random.randint(60,101)
+        if randNum > 89:
+            print(f"Score: {randNum}, your grade is A")
+        elif randNum > 79 and randNum < 90:
+            print(f"Score: {randNum}, your grade is B")
+        elif randNum > 69 and randNum < 80:
+            print(f"Score: {randNum}, your grade is C")
+        elif randNum > 59 and randNum < 70:
+            print(f"Score: {randNum}, your grade is D")
+        else:
+            print("You failed!")
+
+
+grades()
