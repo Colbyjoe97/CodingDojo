c0a19aa81a65a0ecc10c12e212e4551d9abeb7d9
colbyjoe97 <dumonchelle123@gmail.com>
2021-09-16 15:19:50 -0400

    finished part 2

--- Python(2021)/fundamentals/stars.py
+++ Python(2021)/fundamentals/stars.py
@@ -3,3 +3,12 @@ def draw_stars(list):
         print("*" * i)
 
 draw_stars([4,6,1,3,5,7,25])
+
+def stars2(list):
+    for i in list:
+        if isinstance(i, int):
+            print("*" * i)
+        elif isinstance(i, str):
+            print(i[0] * len(i))
+
+stars2(["Hello",2,"test", "colby", 5])
