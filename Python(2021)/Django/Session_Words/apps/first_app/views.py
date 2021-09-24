7d4ce56616846bfcfd280449783d7c72f27df1af
colbyjoe97 <dumonchelle123@gmail.com>
2021-09-24 16:54:57 -0400

    fixed assignment

--- Python(2021)/Django/Session_Words/apps/first_app/views.py
+++ Python(2021)/Django/Session_Words/apps/first_app/views.py
@@ -2,17 +2,40 @@ from django.shortcuts import render, HttpResponse, redirect
 from time import gmtime, strftime
 # Create your views here.
 def index(request):
-    return render(request, "index.html")
+    if 'all_words' in request.session:
+        if 'list'in request.session:
+            request.session['list'] = request.session['all_words']
+            # request.session['list'].append(request.session['word'])
+        else:
+            request.session['list'] = []
+    else:
+        request.session['all_words'] = []
+    context = {
+        "list": request.session['list']
+    }
+    return render(request, "index.html", context)
 
 
 def add_word(request):
-    request.session['word'] = request.POST['word']
-    request.session['time'] = strftime("%b %d, %Y %H:%M %p", gmtime())
-    if 'big_font' in request.POST:
-        if request.POST['big_font'] != "xx-large":
-            request.session['font'] = "small"
-        else:
-            request.session['font'] = request.POST['big_font']
-            
-    request.session['color'] = request.POST['color']
+    if 'font' in request.POST:
+        request.session['font'] = request.POST['font']
+    else:
+        request.session['font'] = "small"
+    request.session['word'] = {
+        'word': request.POST['word'],
+        'color': request.POST['color'],
+        'size': request.session['font'],
+        'time': strftime("%b %d, %Y %H:%M %p", gmtime())
+    }
+
+    if 'all_words' in request.session:
+        request.session['all_words'].append(request.session['word'])
+    else:
+        request.session['all_words'] = []
+        
     return redirect('/')
+
+
+def clear(request):
+    request.session['all_words'] = []
+    return redirect("/")
