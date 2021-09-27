847923d28c1c5fcf5a735b3838188ecc51166b9d
colbyjoe97 <dumonchelle123@gmail.com>
2021-09-27 12:32:16 -0400

    Finished assignment

--- Python(2021)/Django/Ninja_Gold/apps/my_app/views.py
+++ Python(2021)/Django/Ninja_Gold/apps/my_app/views.py
@@ -1,5 +1,54 @@
 from django.shortcuts import render, redirect
-
+from random import randint
 # Create your views here.
 def index(request):
+    if 'gold' not in request.session:
+        request.session['gold'] = 0
+
+    if 'activities' not in request.session:
+        request.session['activities'] = []
+    else:
+        request.session['activities'].reverse()
     return render(request, "index.html")
+
+
+def process(request):
+    if request.POST['location'] == "farm":
+        int = randint(10,20)
+        request.session['gold'] += int
+        request.session['activities'].append({
+            'gold': int,
+            'location': 'Farm',
+        })
+    elif request.POST['location'] == "cave":
+        int = randint(5,10)
+        request.session['gold'] += int
+        request.session['activities'].append({
+            'gold': int,
+            'location': 'Cave',
+            'status': 'good'
+        })
+    elif request.POST['location'] == "house":
+        int = randint(2,5)
+        request.session['gold'] += int
+        request.session['activities'].append({
+            'gold': int,
+            'location': 'House',
+            'status': 'good'
+        })
+    elif request.POST['location'] == "casino":
+        int = randint(-50, 50)
+        status = ""
+        if int >= 0:
+            status = "good"
+        else:
+            status = "bad"
+        request.session['gold'] += int
+        request.session['activities'].append({
+            'gold': int,
+            'location': 'Casino',
+            'status': status
+        })
+
+
+    return redirect("/")
