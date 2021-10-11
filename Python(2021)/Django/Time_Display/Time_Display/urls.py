471435980bbf8b7d22a0e114c5cd076fabb425b2
colbyjoe97 <dumonchelle123@gmail.com>
2021-10-11 17:27:06 -0400

    updated assignment

--- Python(2021)/Django/Time_Display/Time_Display/urls.py
+++ Python(2021)/Django/Time_Display/Time_Display/urls.py
@@ -14,8 +14,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.conf.urls import url, include
-from django.urls import path
 
 urlpatterns = [
-    url(r'^', include('apps.time_app.urls')),
+    url(r'^', include("apps.time_app.urls")),
 ]
