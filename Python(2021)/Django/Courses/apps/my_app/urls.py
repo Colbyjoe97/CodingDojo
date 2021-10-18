141b5fa81b33c4e1147896fce6f0e6d15e0e2d26
colbyjoe97 <dumonchelle123@gmail.com>
2021-10-18 15:32:25 -0400

    Allow users to create courses

--- Python(2021)/Django/Courses/apps/my_app/urls.py
+++ Python(2021)/Django/Courses/apps/my_app/urls.py
@@ -1,5 +1,8 @@
 from django.conf.urls import url
 from . import views
 urlpatterns = [
-    url(r'^$', views.index)
+    url(r'^$', views.index),
+    url(r'^create$', views.courses, name="courses_create"),
+    url(r'^courses/delete/(?P<id>\d+)$', views.destroy_page, name="courses_destroy_page"),
+    url(r'^courses/(?P<id>\d+)/delete$', views.destroy, name="courses_destroy")
 ]
--- Python(2021)/Django/Courses/apps/my_app/views.py
+++ Python(2021)/Django/Courses/apps/my_app/views.py
@@ -1,8 +1,26 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
-
-from django.shortcuts import render
+from models import *
+from django.shortcuts import render, redirect
 
 # Create your views here.
 def index(request):
-    return render(request, 'index.html')
+    context = {
+        'courses': Course.objects.all()
+    }
+    return render(request, 'index.html', context)
+
+def courses(request):
+    course = Course.objects.create(name=request.POST['name'], desc=request.POST['desc'])
+    return redirect("/")
+
+def destroy_page(request, id):
+    context = {
+        'course': Course.objects.get(id=id)
+    }
+    return render(request, 'destroy.html', context)
+
+def destroy(request, id):
+    course = Course.objects.get(id=id)
+    course.delete()
+    return redirect('/')
