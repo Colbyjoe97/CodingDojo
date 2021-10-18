33d46910020abf919dc84b04ccd6b14c179851fc
colbyjoe97 <dumonchelle123@gmail.com>
2021-10-18 13:23:20 -0400

    Got validations working

--- Python(2021)/Django/Semi_Restful_Users/apps/my_app/models.py
+++ Python(2021)/Django/Semi_Restful_Users/apps/my_app/models.py
@@ -1,13 +1,28 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
-
+import re
 from django.db import models
 
 # Create your models here.
+class UserManager(models.Manager):
+    def basic_validator(self, postData):
+        EMAIL_REGEX = re.compile(r'^[a-zA-Z0-9.+_-]+@[a-zA-Z0-9._-]+\.[a-zA-Z]+$')
+        errors = {}
+        if len(postData['first_name']) < 2:
+            errors['fname'] = "First Name must be at least 3 characters long"
+        if len(postData['last_name']) < 2:
+            errors['lname'] = "Last Name must be at least 3 characters long"
+        if len(postData['email']) < 1:
+            errors['email'] = "Email is required"
+        elif not EMAIL_REGEX.match(postData['email']):
+            errors['emailPattern'] = "Email is not valid"
+        
+        return errors
+
 class User(models.Model):
     first_name=models.CharField(max_length=255)
     last_name=models.CharField(max_length=255)
     email=models.CharField(max_length=255)
     created_at = models.DateTimeField(auto_now_add = True)
     updated_at = models.DateTimeField(auto_now = True)
-
+    objects = UserManager()
--- Python(2021)/Django/Semi_Restful_Users/apps/my_app/views.py
+++ Python(2021)/Django/Semi_Restful_Users/apps/my_app/views.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 from django.shortcuts import render, redirect
+from django.contrib import messages
 from models import *
 # Create your views here.
 def index(request):
@@ -23,8 +24,14 @@ def new(request):
     return render(request, 'new.html')
 
 def create(request):
-    User.objects.create(first_name=request.POST['first_name'], last_name=request.POST['last_name'], email=request.POST['email'])
-    return redirect("/")
+    errors = User.objects.basic_validator(request.POST)
+    if len(errors) > 0:
+        for tag, error in errors.iteritems():
+            messages.error(request, error)
+        return redirect('/users/new')
+    else:
+        User.objects.create(first_name=request.POST['first_name'], last_name=request.POST['last_name'], email=request.POST['email'])
+        return redirect("/")
 
 def edit(request, id):
     user = User.objects.get(id=id)
@@ -35,11 +42,17 @@ def edit(request, id):
 
 def update(request, id):
     user = User.objects.get(id=id)
-    user.first_name=request.POST['first_name']
-    user.last_name=request.POST['last_name']
-    user.email=request.POST['email']
-    user.save()
-    return redirect("/users")
+    errors = User.objects.basic_validator(request.POST)
+    if len(errors) > 0:
+        for tag, error in errors.iteritems():
+            messages.error(request, error)
+        return redirect('/users/{}/edit'.format(user.id))
+    else:
+        user.first_name=request.POST['first_name']
+        user.last_name=request.POST['last_name']
+        user.email=request.POST['email']
+        user.save()
+        return redirect("/users")
 
 def destroy(request, id):
     user = User.objects.get(id=id)
