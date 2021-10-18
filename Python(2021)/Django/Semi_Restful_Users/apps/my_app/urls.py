9d9f6487009785951482a220a533e13a0962f480
colbyjoe97 <dumonchelle123@gmail.com>
2021-10-18 13:04:54 -0400

    got full CRUD working

--- Python(2021)/Django/Semi_Restful_Users/apps/my_app/urls.py
+++ Python(2021)/Django/Semi_Restful_Users/apps/my_app/urls.py
@@ -2,5 +2,11 @@ from django.conf.urls import url
 from . import views
 urlpatterns = [
     url(r'^$', views.index),
-    url(r'users', views.users)
+    url(r'^users$', views.users),
+    url(r'^users/new', views.new, name="new_user"),
+    url(r'^create', views.create),
+    url(r'^users/(?P<id>\d+)$', views.show, name="users_show"),
+    url(r'^users/(?P<id>\d+)/edit$', views.edit, name="users_edit"),
+    url(r'^users/(?P<id>\d+)/update$', views.update),
+    url(r'^users/(?P<id>\d+)/destroy$', views.destroy, name='users_destroy')
 ]
--- Python(2021)/Django/Semi_Restful_Users/apps/my_app/views.py
+++ Python(2021)/Django/Semi_Restful_Users/apps/my_app/views.py
@@ -1,11 +1,47 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
-
 from django.shortcuts import render, redirect
-
+from models import *
 # Create your views here.
 def index(request):
     return redirect('/users')
 
 def users(request):
-    return render(request, 'index.html')
+    context = {
+        'users': User.objects.all()
+    }
+    return render(request, 'index.html', context)
+
+def show(request, id):
+    user = User.objects.get(id=id)
+    context = {
+        'user': user
+    }
+    return render(request, 'show.html', context)
+
+def new(request):
+    return render(request, 'new.html')
+
+def create(request):
+    User.objects.create(first_name=request.POST['first_name'], last_name=request.POST['last_name'], email=request.POST['email'])
+    return redirect("/")
+
+def edit(request, id):
+    user = User.objects.get(id=id)
+    context = {
+        'user': user
+    }
+    return render(request, 'edit.html', context)
+
+def update(request, id):
+    user = User.objects.get(id=id)
+    user.first_name=request.POST['first_name']
+    user.last_name=request.POST['last_name']
+    user.email=request.POST['email']
+    user.save()
+    return redirect("/users")
+
+def destroy(request, id):
+    user = User.objects.get(id=id)
+    user.delete()
+    return redirect('/users')
